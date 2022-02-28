50191b4b5f582bce699f2f9a0e5bbddc8bbbdecd
shubham <shubhampatel2295@gmail.com>
2022-02-28 14:23:42 -0700

    UI

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.instructions').textContent = "🎉 Hurray you guessed it correct.....";
+//document.querySelector('.number').textContent = 2;
+document.querySelector('.score').textContent = 11;
